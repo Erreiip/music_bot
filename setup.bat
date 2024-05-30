776d137c79c8d59ebf7784f39b0dfcee11b3d8d9
erreiip <lolpierre154@gmail.com>
2024-05-30 11:43:19 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
-setx DATABASE_URL "postgresql://ytb:sheshGaming@db:5432/youtube" -m
-setx DISCORD_TOKEN "MTIzNDU0OTc4MDUwNDY0NTcwNQ.Gmh42V.oMFyG4LAn84LXbmbxyn4PEp0rG-Wt_MH4K9yHk" -m
-setx YTB_TOKEN "AIzaSyAq0yKDe2x_Wb5Yb6quZvR9gVIr2WazcNU" -m
-setx SPOTIFY_PB "d84f5f1b34514f5d889f2c3e955d6331" -m
-setx SPOTIFY_PV "712391b485754f2e8c3528486a8eacae" -m
-setx DEBUG "false" -m
+setx DATABASE_URL "postgresql://ytb:sheshGaming@db:5432/youtube"
+setx DISCORD_TOKEN "MTIzNDU0OTc4MDUwNDY0NTcwNQ.Gmh42V.oMFyG4LAn84LXbmbxyn4PEp0rG-Wt_MH4K9yHk"
+setx YTB_TOKEN "AIzaSyAq0yKDe2x_Wb5Yb6quZvR9gVIr2WazcNU"
+setx SPOTIFY_PB "d84f5f1b34514f5d889f2c3e955d6331"
+setx SPOTIFY_PV "712391b485754f2e8c3528486a8eacae"
+setx DEBUG "true"
